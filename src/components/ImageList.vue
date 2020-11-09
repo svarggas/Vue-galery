4c82f4d00c83062dcf5a256dc5ebf1a89b3e0af0
Sebastian Vargas Rojas <s.varggas@gmail.com>
2020-11-08 19:50:00 -0600

    image list
    - Validation if the user dont have images uploaded

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,9 +1,15 @@
 <template>
     <div>
-        <div v-if="isLoggedIn" class="image-container">
-            <img v-for="image in allImages" 
+        <div v-if="isLoggedIn">
+            <div v-if="allImages.length != 0" class="image-container">
+                <img v-for="image in allImages" 
                 :src="image.link" 
                 :key="image.id"/>
+            </div>
+            <div v-else class="questionMarkContiner">
+                <span class="questionMark">?</span><br/>
+                <h1>There are no images uploaded right now!</h1>    
+            </div>
         </div>
         <div v-else>  
             <ErrorPage/>
@@ -12,20 +18,20 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-import ErrorPage from './ErrorPage'
+    import { mapActions, mapGetters } from 'vuex'
+    import ErrorPage from './ErrorPage'
 
-export default {
-    name: 'ImageList',
-    methods: mapActions(['fetchImages']),
-    computed: mapGetters(['allImages', 'isLoggedIn']),
-    components: {
-        ErrorPage
-    },
-    created(){
-        this.fetchImages();
+    export default {
+        name: 'ImageList',
+        methods: mapActions(['fetchImages']),
+        computed: mapGetters(['allImages', 'isLoggedIn']),
+        components: {
+            ErrorPage
+        },
+        created(){
+            this.fetchImages();
+        }
     }
-}
 </script>
 
 <style scoped>
@@ -37,5 +43,15 @@ export default {
     .image-container{
         column-count: 3;
         column-gap: 0;
+    }
+    .questionMarkContiner{
+        text-align: center;
+        padding-top: 20%;
+    }
+    .questionMark{
+        color: lightblue;
+        font-size: 400px;
+        font-weight: 1000;
+        text-decoration: italic;
     }
 </style>
