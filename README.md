a003f58bab0a733b7fcff782b90f9233a0db53f8
Sebastian Vargas Rojas <s.varggas@gmail.com>
2021-05-11 10:53:18 -0600

    Quick fix

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# galery
-Basic galery create with Vue, Vuex and Vue-router.<br/>
-Powered by IMGUR (you need an IMGUR account to access)
+# IMGUR Galery :framed_picture:
+Basic galery created with Vue, Vuex and Vue-router<br/>
+Powered by IMGUR API: https://imgur.com, you need an IMGUR account to access
 
 Visit: https://imgur-galery.netlify.app
--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,108 +1,114 @@
 <template>
-    <div v-if="isLoggedIn">
-        <div class="dropper">
-            <input type="file" class="fileUp"
-            @change="previewFiles($event.target.files);uploadImages($event.target.files);"
-            multiple 
-            accept="image/*"/>
-            <div class="textUploader">
-                <i class="upload huge icon uploader"></i>
-                <br/>
-                <h1 class="uploader">Drag and drop or browse files!</h1> 
-            </div>
-        </div>   
-        <h2>Files attached</h2>
-        <table class="ui fixed celled table striped">
-            <thead>
-                <th>#</th>
-                <th>Name</th>
-                <th>Size</th>
-                <th>Type</th>
-            </thead>
-            <tbody>
-                <tr v-for="file in arrayFiles" v-bind:key="file.id">
-                    <td>{{ file.id }}</td>
-                    <td>{{ file.name }}</td>
-                    <td>{{ file.size }}</td>
-                    <td>{{ file.type }}</td>
-                </tr>
-            </tbody>
-        </table> 
+  <div v-if="isLoggedIn">
+    <div class="dropper">
+      <input
+        type="file"
+        class="fileUp"
+        @change="
+          previewFiles($event.target.files);
+          uploadImages($event.target.files);
+        "
+        multiple
+        accept="image/*"
+      />
+      <div class="textUploader">
+        <i class="upload huge icon uploader"></i>
+        <br />
+        <h1 class="uploader">Drag and drop or browse files!</h1>
+      </div>
     </div>
-    
-    <div v-else>
-        <ErrorPage/>
-    </div>  
+    <h2>Files attached</h2>
+    <table class="ui fixed celled table striped">
+      <thead>
+        <th>#</th>
+        <th>Name</th>
+        <th>Size</th>
+        <th>Type</th>
+      </thead>
+      <tbody>
+        <tr v-for="file in arrayFiles" v-bind:key="file.id">
+          <td>{{ file.id }}</td>
+          <td>{{ file.name }}</td>
+          <td>{{ file.size }}</td>
+          <td>{{ file.type }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <div v-else>
+    <ErrorPage />
+  </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
-import ErrorPage from './ErrorPage'
+import { mapActions, mapGetters } from "vuex";
+import ErrorPage from "./ErrorPage";
 
 export default {
-    name: 'UploadForm',
-    data() {
-        return {
-            arrayFiles: [],
-            showFiles: false
-        }
+  name: "UploadForm",
+  data() {
+    return {
+      arrayFiles: [],
+      showFiles: false,
+    };
+  },
+  methods: {
+    ...mapActions(["uploadImages"]),
+    previewFiles: function (event) {
+      let temp,
+        counter = 1;
+      event.forEach((file) => {
+        temp = {
+          id: counter,
+          name: file["name"],
+          size: file["size"] + " bytes",
+          type: file["type"],
+        };
+        this.$set(this.arrayFiles, this.arrayFiles.length, temp);
+        counter++;
+      });
+      this.showFiles = true;
     },
-    methods: {
-        ...mapActions (['uploadImages']),
-        previewFiles: function (event) {
-            let temp, counter = 1
-            event.forEach( file => {
-                temp = {
-                    id: counter,
-                    name: file['name'],
-                    size: file['size'] + ' bytes',
-                    type: file['type']
-                }
-                this.$set(this.arrayFiles, this.arrayFiles.length, temp)
-                counter++
-            });
-            this.showFiles = true
-            console.log(this.$data.arrayFiles)
-        }
-    },
-    computed: mapGetters(['isLoggedIn']),
-    components: {
-        ErrorPage
-    }
-}
+  },
+  computed: mapGetters(["isLoggedIn"]),
+  components: {
+    ErrorPage,
+  },
+};
 </script>
 
 <style scoped>
-    .dropper{
-        height: 30vh;
-        border: 2px dashed;
-        border-radius: 5px;
-        justify-content: center;
-        align-items: center;
-        display: flex;
-        position: relative;
-        margin-top: 15px;
-    }
-    .droppper:hover{
-        background: #eee;
-    }
-    input{
-        width: 100%;
-        height: 30vh;
-        position: absolute;
-        opacity: 0;
-    }
-    .uploader{
-        color: lightblue;
-    }
-    .textUploader{
-        text-align: center;
-    }
-    .table{
-        margin-top: 15px;
-        width: 100%;
-    }
-    .nothing{
-        text-align: center !important;
-    }
+.dropper {
+  height: 30vh;
+  border: 2px dashed;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  position: relative;
+  margin-top: 15px;
+}
+.droppper:hover {
+  background: #eee;
+}
+input {
+  width: 100%;
+  height: 30vh;
+  position: absolute;
+  opacity: 0;
+}
+.uploader {
+  color: lightblue;
+}
+.textUploader {
+  text-align: center;
+}
+.table {
+  margin-top: 15px;
+  width: 100%;
+}
+.nothing {
+  text-align: center !important;
+}
 </style>
